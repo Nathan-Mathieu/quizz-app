0307c31fc04e5f852dd2800c6b9769234b3c451d
Nathan-Mathieu <nathan.mathieu@gmail.com>
2021-01-06 15:00:45 +0100

    0601

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -86,14 +86,520 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./node_modules/css-loader/dist/cjs.js!./src/style.css":
+/*!*************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./src/style.css ***!
+  \*************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0__);
+// Imports
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(true);
+// Module
+___CSS_LOADER_EXPORT___.push([module.i, "/* reset */\r\n*{\r\n      margin: 0;\r\n      padding: 0;\r\n      box-sizing: border-box;\r\n}\r\n\r\n/* variables */\r\n:root{\r\n      --main : #ff4d4d;\r\n}\r\n\r\n/* main  */\r\nbody{\r\n      background: var(--main);\r\n      font-family: 'Open Sans', sans-serif;   \r\n}\r\n\r\nh1,h2{\r\n      color: white;\r\n      text-align: center;\r\n      margin-top: 2rem;\r\n      font-size: 3rem;\r\n      font-weight: 500;\r\n}\r\nh2{ \r\n      margin-top: 0.5rem;\r\n      font-size: 2rem;\r\n      font-weight: 300;\r\n}\r\nh4{\r\n      margin-bottom: 1rem;\r\n}\r\n.question-block{\r\n      background: white;\r\n      width: 50%;\r\n      margin: 2rem auto 1rem auto;\r\n      padding: 1rem;\r\n      border-radius: 10px;\r\n      box-shadow: 5px 5px 5px 0 rgba(17, 17, 17, 0.39);\r\n}\r\n\r\nform button{\r\n      display: block;\r\n      margin: 1rem auto 3rem auto;\r\n      padding: 0.5rem 1rem 0.5rem 1rem;\r\n      border: none;\r\n      cursor: pointer;\r\n      border-radius: 5px;\r\n}\r\nform button:hover{\r\n      background: rgba(255, 255, 255, 0.733);\r\n}\r\n\r\nform button:focus{\r\n      outline: none;\r\n}\r\n\r\n.results{\r\n      margin: 1rem auto 3rem auto;\r\n      color: white;\r\n      width: 50%;\r\n      text-align: center;\r\n      font-size : 2rem;\r\n}\r\n\r\n@media only screen and (max-width: 1001px) {\r\n      .question-block{\r\n            width: 80%;   \r\n      }\r\n}", "",{"version":3,"sources":["webpack://src/style.css"],"names":[],"mappings":"AAAA,UAAU;AACV;MACM,SAAS;MACT,UAAU;MACV,sBAAsB;AAC5B;;AAEA,cAAc;AACd;MACM,gBAAgB;AACtB;;AAEA,UAAU;AACV;MACM,uBAAuB;MACvB,oCAAoC;AAC1C;;AAEA;MACM,YAAY;MACZ,kBAAkB;MAClB,gBAAgB;MAChB,eAAe;MACf,gBAAgB;AACtB;AACA;MACM,kBAAkB;MAClB,eAAe;MACf,gBAAgB;AACtB;AACA;MACM,mBAAmB;AACzB;AACA;MACM,iBAAiB;MACjB,UAAU;MACV,2BAA2B;MAC3B,aAAa;MACb,mBAAmB;MACnB,gDAAgD;AACtD;;AAEA;MACM,cAAc;MACd,2BAA2B;MAC3B,gCAAgC;MAChC,YAAY;MACZ,eAAe;MACf,kBAAkB;AACxB;AACA;MACM,sCAAsC;AAC5C;;AAEA;MACM,aAAa;AACnB;;AAEA;MACM,2BAA2B;MAC3B,YAAY;MACZ,UAAU;MACV,kBAAkB;MAClB,gBAAgB;AACtB;;AAEA;MACM;YACM,UAAU;MAChB;AACN","sourcesContent":["/* reset */\r\n*{\r\n      margin: 0;\r\n      padding: 0;\r\n      box-sizing: border-box;\r\n}\r\n\r\n/* variables */\r\n:root{\r\n      --main : #ff4d4d;\r\n}\r\n\r\n/* main  */\r\nbody{\r\n      background: var(--main);\r\n      font-family: 'Open Sans', sans-serif;   \r\n}\r\n\r\nh1,h2{\r\n      color: white;\r\n      text-align: center;\r\n      margin-top: 2rem;\r\n      font-size: 3rem;\r\n      font-weight: 500;\r\n}\r\nh2{ \r\n      margin-top: 0.5rem;\r\n      font-size: 2rem;\r\n      font-weight: 300;\r\n}\r\nh4{\r\n      margin-bottom: 1rem;\r\n}\r\n.question-block{\r\n      background: white;\r\n      width: 50%;\r\n      margin: 2rem auto 1rem auto;\r\n      padding: 1rem;\r\n      border-radius: 10px;\r\n      box-shadow: 5px 5px 5px 0 rgba(17, 17, 17, 0.39);\r\n}\r\n\r\nform button{\r\n      display: block;\r\n      margin: 1rem auto 3rem auto;\r\n      padding: 0.5rem 1rem 0.5rem 1rem;\r\n      border: none;\r\n      cursor: pointer;\r\n      border-radius: 5px;\r\n}\r\nform button:hover{\r\n      background: rgba(255, 255, 255, 0.733);\r\n}\r\n\r\nform button:focus{\r\n      outline: none;\r\n}\r\n\r\n.results{\r\n      margin: 1rem auto 3rem auto;\r\n      color: white;\r\n      width: 50%;\r\n      text-align: center;\r\n      font-size : 2rem;\r\n}\r\n\r\n@media only screen and (max-width: 1001px) {\r\n      .question-block{\r\n            width: 80%;   \r\n      }\r\n}"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ __webpack_exports__["default"] = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./node_modules/css-loader/dist/runtime/api.js":
+/*!*****************************************************!*\
+  !*** ./node_modules/css-loader/dist/runtime/api.js ***!
+  \*****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Tobias Koppers @sokra
+*/
+// css base code, injected by the css-loader
+// eslint-disable-next-line func-names
+module.exports = function (useSourceMap) {
+  var list = []; // return the list of modules as css string
+
+  list.toString = function toString() {
+    return this.map(function (item) {
+      var content = cssWithMappingToString(item, useSourceMap);
+
+      if (item[2]) {
+        return "@media ".concat(item[2], " {").concat(content, "}");
+      }
+
+      return content;
+    }).join('');
+  }; // import a list of modules into the list
+  // eslint-disable-next-line func-names
+
+
+  list.i = function (modules, mediaQuery, dedupe) {
+    if (typeof modules === 'string') {
+      // eslint-disable-next-line no-param-reassign
+      modules = [[null, modules, '']];
+    }
+
+    var alreadyImportedModules = {};
+
+    if (dedupe) {
+      for (var i = 0; i < this.length; i++) {
+        // eslint-disable-next-line prefer-destructuring
+        var id = this[i][0];
+
+        if (id != null) {
+          alreadyImportedModules[id] = true;
+        }
+      }
+    }
+
+    for (var _i = 0; _i < modules.length; _i++) {
+      var item = [].concat(modules[_i]);
+
+      if (dedupe && alreadyImportedModules[item[0]]) {
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+
+      if (mediaQuery) {
+        if (!item[2]) {
+          item[2] = mediaQuery;
+        } else {
+          item[2] = "".concat(mediaQuery, " and ").concat(item[2]);
+        }
+      }
+
+      list.push(item);
+    }
+  };
+
+  return list;
+};
+
+function cssWithMappingToString(item, useSourceMap) {
+  var content = item[1] || ''; // eslint-disable-next-line prefer-destructuring
+
+  var cssMapping = item[3];
+
+  if (!cssMapping) {
+    return content;
+  }
+
+  if (useSourceMap && typeof btoa === 'function') {
+    var sourceMapping = toComment(cssMapping);
+    var sourceURLs = cssMapping.sources.map(function (source) {
+      return "/*# sourceURL=".concat(cssMapping.sourceRoot || '').concat(source, " */");
+    });
+    return [content].concat(sourceURLs).concat([sourceMapping]).join('\n');
+  }
+
+  return [content].join('\n');
+} // Adapted from convert-source-map (MIT)
+
+
+function toComment(sourceMap) {
+  // eslint-disable-next-line no-undef
+  var base64 = btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap))));
+  var data = "sourceMappingURL=data:application/json;charset=utf-8;base64,".concat(base64);
+  return "/*# ".concat(data, " */");
+}
+
+/***/ }),
+
+/***/ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js":
+/*!****************************************************************************!*\
+  !*** ./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js ***!
+  \****************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var isOldIE = function isOldIE() {
+  var memo;
+  return function memorize() {
+    if (typeof memo === 'undefined') {
+      // Test for IE <= 9 as proposed by Browserhacks
+      // @see http://browserhacks.com/#hack-e71d8692f65334173fee715c222cb805
+      // Tests for existence of standard globals is to allow style-loader
+      // to operate correctly into non-standard environments
+      // @see https://github.com/webpack-contrib/style-loader/issues/177
+      memo = Boolean(window && document && document.all && !window.atob);
+    }
+
+    return memo;
+  };
+}();
+
+var getTarget = function getTarget() {
+  var memo = {};
+  return function memorize(target) {
+    if (typeof memo[target] === 'undefined') {
+      var styleTarget = document.querySelector(target); // Special case to return head of iframe instead of iframe itself
+
+      if (window.HTMLIFrameElement && styleTarget instanceof window.HTMLIFrameElement) {
+        try {
+          // This will throw an exception if access to iframe is blocked
+          // due to cross-origin restrictions
+          styleTarget = styleTarget.contentDocument.head;
+        } catch (e) {
+          // istanbul ignore next
+          styleTarget = null;
+        }
+      }
+
+      memo[target] = styleTarget;
+    }
+
+    return memo[target];
+  };
+}();
+
+var stylesInDom = [];
+
+function getIndexByIdentifier(identifier) {
+  var result = -1;
+
+  for (var i = 0; i < stylesInDom.length; i++) {
+    if (stylesInDom[i].identifier === identifier) {
+      result = i;
+      break;
+    }
+  }
+
+  return result;
+}
+
+function modulesToDom(list, options) {
+  var idCountMap = {};
+  var identifiers = [];
+
+  for (var i = 0; i < list.length; i++) {
+    var item = list[i];
+    var id = options.base ? item[0] + options.base : item[0];
+    var count = idCountMap[id] || 0;
+    var identifier = "".concat(id, " ").concat(count);
+    idCountMap[id] = count + 1;
+    var index = getIndexByIdentifier(identifier);
+    var obj = {
+      css: item[1],
+      media: item[2],
+      sourceMap: item[3]
+    };
+
+    if (index !== -1) {
+      stylesInDom[index].references++;
+      stylesInDom[index].updater(obj);
+    } else {
+      stylesInDom.push({
+        identifier: identifier,
+        updater: addStyle(obj, options),
+        references: 1
+      });
+    }
+
+    identifiers.push(identifier);
+  }
+
+  return identifiers;
+}
+
+function insertStyleElement(options) {
+  var style = document.createElement('style');
+  var attributes = options.attributes || {};
+
+  if (typeof attributes.nonce === 'undefined') {
+    var nonce =  true ? __webpack_require__.nc : undefined;
+
+    if (nonce) {
+      attributes.nonce = nonce;
+    }
+  }
+
+  Object.keys(attributes).forEach(function (key) {
+    style.setAttribute(key, attributes[key]);
+  });
+
+  if (typeof options.insert === 'function') {
+    options.insert(style);
+  } else {
+    var target = getTarget(options.insert || 'head');
+
+    if (!target) {
+      throw new Error("Couldn't find a style target. This probably means that the value for the 'insert' parameter is invalid.");
+    }
+
+    target.appendChild(style);
+  }
+
+  return style;
+}
+
+function removeStyleElement(style) {
+  // istanbul ignore if
+  if (style.parentNode === null) {
+    return false;
+  }
+
+  style.parentNode.removeChild(style);
+}
+/* istanbul ignore next  */
+
+
+var replaceText = function replaceText() {
+  var textStore = [];
+  return function replace(index, replacement) {
+    textStore[index] = replacement;
+    return textStore.filter(Boolean).join('\n');
+  };
+}();
+
+function applyToSingletonTag(style, index, remove, obj) {
+  var css = remove ? '' : obj.media ? "@media ".concat(obj.media, " {").concat(obj.css, "}") : obj.css; // For old IE
+
+  /* istanbul ignore if  */
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = replaceText(index, css);
+  } else {
+    var cssNode = document.createTextNode(css);
+    var childNodes = style.childNodes;
+
+    if (childNodes[index]) {
+      style.removeChild(childNodes[index]);
+    }
+
+    if (childNodes.length) {
+      style.insertBefore(cssNode, childNodes[index]);
+    } else {
+      style.appendChild(cssNode);
+    }
+  }
+}
+
+function applyToTag(style, options, obj) {
+  var css = obj.css;
+  var media = obj.media;
+  var sourceMap = obj.sourceMap;
+
+  if (media) {
+    style.setAttribute('media', media);
+  } else {
+    style.removeAttribute('media');
+  }
+
+  if (sourceMap && btoa) {
+    css += "\n/*# sourceMappingURL=data:application/json;base64,".concat(btoa(unescape(encodeURIComponent(JSON.stringify(sourceMap)))), " */");
+  } // For old IE
+
+  /* istanbul ignore if  */
+
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    while (style.firstChild) {
+      style.removeChild(style.firstChild);
+    }
+
+    style.appendChild(document.createTextNode(css));
+  }
+}
+
+var singleton = null;
+var singletonCounter = 0;
+
+function addStyle(obj, options) {
+  var style;
+  var update;
+  var remove;
+
+  if (options.singleton) {
+    var styleIndex = singletonCounter++;
+    style = singleton || (singleton = insertStyleElement(options));
+    update = applyToSingletonTag.bind(null, style, styleIndex, false);
+    remove = applyToSingletonTag.bind(null, style, styleIndex, true);
+  } else {
+    style = insertStyleElement(options);
+    update = applyToTag.bind(null, style, options);
+
+    remove = function remove() {
+      removeStyleElement(style);
+    };
+  }
+
+  update(obj);
+  return function updateStyle(newObj) {
+    if (newObj) {
+      if (newObj.css === obj.css && newObj.media === obj.media && newObj.sourceMap === obj.sourceMap) {
+        return;
+      }
+
+      update(obj = newObj);
+    } else {
+      remove();
+    }
+  };
+}
+
+module.exports = function (list, options) {
+  options = options || {}; // Force single-tag solution on IE6-9, which has a hard limit on the # of <style>
+  // tags it will allow on a page
+
+  if (!options.singleton && typeof options.singleton !== 'boolean') {
+    options.singleton = isOldIE();
+  }
+
+  list = list || [];
+  var lastIdentifiers = modulesToDom(list, options);
+  return function update(newList) {
+    newList = newList || [];
+
+    if (Object.prototype.toString.call(newList) !== '[object Array]') {
+      return;
+    }
+
+    for (var i = 0; i < lastIdentifiers.length; i++) {
+      var identifier = lastIdentifiers[i];
+      var index = getIndexByIdentifier(identifier);
+      stylesInDom[index].references--;
+    }
+
+    var newLastIdentifiers = modulesToDom(newList, options);
+
+    for (var _i = 0; _i < lastIdentifiers.length; _i++) {
+      var _identifier = lastIdentifiers[_i];
+
+      var _index = getIndexByIdentifier(_identifier);
+
+      if (stylesInDom[_index].references === 0) {
+        stylesInDom[_index].updater();
+
+        stylesInDom.splice(_index, 1);
+      }
+    }
+
+    lastIdentifiers = newLastIdentifiers;
+  };
+};
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ "./src/style.css");
+/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_style_css__WEBPACK_IMPORTED_MODULE_0__);
+
+var form = document.querySelector(".form-quizz");
+var tabResults = ["b", "a", "a", "c", "b"];
+var results = document.querySelector(".results");
+var tabReponsesUser = [];
+var nbFautes = 0;
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  for (var i = 1; i < 6; i++) {
+    tabReponsesUser.push(document.querySelector("input[name=\"q".concat(i, "\"]:checked")).value);
+  }
+
+  verifResults();
+});
+
+function verifResults() {
+  console.log("Results : ", tabResults);
+  console.log("User : ", tabReponsesUser);
+
+  for (var i = 0; i < 6; i++) {
+    if (tabReponsesUser[i] != tabResults[i]) {
+      nbFautes += 1;
+    }
+  }
+
+  displayResults(nbFautes);
+}
+
+function displayResults(fautes) {
+  console.log("displayresults");
+
+  switch (fautes) {
+    case 0:
+      results.innerHTML = "Bravo ! Aucune faute ! 👑 ";
+      break;
+
+    case 1:
+      results.innerHTML = "On y est presque ! 1 faute ! 😃";
+      break;
+
+    case 2:
+      results.innerHTML = "Pas mal ! 2 faute ! 🙂";
+      break;
+
+    case 3:
+      results.innerHTML = "3 fautes ! Tu peux mieux faire ! 💪";
+      break;
+
+    case 4:
+      results.innerHTML = "4 fautes ! C'est pas terrible... 👎";
+      break;
+
+    case 5:
+      results.innerHTML = "5 fautes ! Tu n'as pas honte ? 💩";
+      break;
+  }
+
+  window.scrollTo({
+    top: 1000,
+    left: 0,
+    behavior: "smooth"
+  });
+  setTimeout(function () {
+    location.reload();
+  }, '3000');
+}
+
+/***/ }),
+
+/***/ "./src/style.css":
+/*!***********************!*\
+  !*** ./src/style.css ***!
+  \***********************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
+
+var api = __webpack_require__(/*! ../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+            var content = __webpack_require__(/*! !../node_modules/css-loader/dist/cjs.js!./style.css */ "./node_modules/css-loader/dist/cjs.js!./src/style.css");
+
+            content = content.__esModule ? content.default : content;
+
+            if (typeof content === 'string') {
+              content = [[module.i, content, '']];
+            }
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = api(content, options);
 
 
+
+module.exports = content.locals || {};
 
 /***/ })
 
